b649c735d3dfcde21f65952b662a663ea93c5d79
aviralAtdrive <aviral.atdrive@gmail.com>
2024-09-11 03:13:18 +0530

    :lipstick: Sidebar UI and :truck: Route to Sidebar pages

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -16,7 +16,7 @@ const Sidebar = () => {
             sidebarLinks.map((link) => {
               const isActive = pathname === link.route || pathname.startsWith(link.route);
               return ( 
-                <Link href="{link.route}" key={link.label} className={cn('flex gap-4 items-center rounded-lg justify-start', {'bg-blue-1': isActive,})}>
+                <Link href={link.route} key={link.label} className={cn('flex gap-4 items-center p-4 rounded-lg justify-start', {'bg-cyan': isActive,})}>
                   <img src={link.imgUrl} alt={link.label} width="24" height="24" />
                   <p className='text-lg font-semibold max-lg:hidden'>
                     {link.label}
